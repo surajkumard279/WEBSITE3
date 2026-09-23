1f76efcdddfbb977f59273299c1544651627ec8d
surajkumard279 <43441453+surajkumard279@users.noreply.github.com>
2026-09-24 00:00:22 +0530

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/',
+    base: '/WEBSITE3/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
